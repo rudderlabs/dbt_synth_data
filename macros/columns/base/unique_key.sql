9589fe884ce959a976c969a34597ff058e73fb68
Gabriel Breahna <gabriel.breahna@gmail.com>
2024-08-14 19:56:35 +0300

    support for unique keys plus null fraction

---
+++ macros/columns/base/unique_key.sql
@@ -0,0 +1,29 @@
+{% macro synth_column_unique_key(name, null_frac=0) -%}
+    {% set unique_field %}
+        {{ adapter.dispatch('synth_column_unique_key')() }}
+    {% endset %}
+
+    {% set base_field %}
+        CASE
+            WHEN {{ dbt_synth_data.synth_distribution_continuous_uniform(min=0.0, max=1.0) }} < {{null_frac}}
+            THEN NULL
+            ELSE {{ unique_field }}
+        END as {{name}}
+    {% endset %}
+
+    {{ dbt_synth_data.synth_store('base_fields', name, base_field) }}
+
+    {% set final_field %}
+        {{name}}
+    {% endset %}
+    {{ dbt_synth_data.synth_store('final_fields', name, final_field) }}
+{%- endmacro %}
+
+{% macro default__synth_column_unique_key() -%}
+    MD5( __row_number::varchar )
+{%- endmacro %}
+
+{% macro sqlite__synth_column_unique_key() %}
+    {# SQLite doesn't support MD5() out-of-the-box, so just use the row number #}
+    __row_number
+{% endmacro %}
--- models/columns_all.sql
+++ models/columns_all.sql
@@ -1,22 +1,9 @@
 {{ config(materialized='table') }}
 
 with
-{{ synth_column_select(name='column_select_original',
-    model_name="synth_words",
-    value_cols="word",
-    distribution="weighted",
-    weight_col="frequency",
-    filter="part_of_speech like '%ADJ%'"
-) }}
-
-{{ synth_column_select(name='column_select_weighted_null',
-    model_name="synth_words",
-    value_cols="word",
-    distribution="uniform",
-    weight_col="frequency",
-    filter="part_of_speech like '%ADJ%'",
-    null_frac=0.2
-) }}
+{{ synth_column_primary_key(name='column_pkey') }}
+{{ synth_column_unique_key(name='column_ukey_std') }}
+{{ synth_column_unique_key(name='column_ukey_null', null_frac=0.2) }}
 
 {{ synth_column_foreign_key(name='column_fkey_std', model_name='stores', column='k_store') }}
 {{ synth_column_foreign_key(name='column_fkey_null', model_name='stores', column='k_store', null_frac=0.2) }}
