6bb65191a4de04fcdea5c35e03ef8f6acd149d4f
Gabriel Breahna <gabriel.breahna@gmail.com>
2024-08-16 10:38:41 +0300

    all columns have nulls, but words

--- models/columns_all.sql
+++ models/columns_all.sql
@@ -8,6 +8,11 @@ with
 {{ synth_column_foreign_key(name='column_fkey_std', model_name='stores', column='k_store') }}
 {{ synth_column_foreign_key(name='column_fkey_null', model_name='stores', column='k_store', null_frac=0.2) }}
 
+{{ synth_column_word(name='column_word_std', language_code="en", distribution="weighted", pos=["NOUN", "VERB"]) }}
+{{ synth_column_word(name='column_word_null', language_code="en", distribution="weighted", pos=["NOUN", "VERB"], null_frac=0.2) }}
+
+{{ synth_column_words(name='column_words_std', language_code="en", distribution="uniform", n=5) }}
+{{ synth_column_words(name='column_words_null', language_code="en", distribution="uniform", n=5) }}
 
 
 
