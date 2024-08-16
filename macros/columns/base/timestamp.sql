5d7bd13c07f7e30610a6a007c23cee51f8da317d
Gabriel Breahna <gabriel.breahna@gmail.com>
2024-08-16 17:30:18 +0300

    added null support for timestamp

--- macros/columns/base/timestamp.sql
+++ macros/columns/base/timestamp.sql
@@ -1,6 +1,6 @@
-{% macro synth_column_timestamp(name, min='2020-01-01 00:00:00.000', max='2030-12-31 23:59:59.999', distribution='uniform') -%}
+{% macro synth_column_timestamp(name, min='2020-01-01 00:00:00.000', max='2030-12-31 23:59:59.999', distribution='uniform', null_frac=0) -%}
     {% set base_field %}
-        {{ adapter.dispatch('synth_column_timestamp_base')(min, max, distribution) }} AS {{name}}
+        {{ adapter.dispatch('synth_column_timestamp_base')(min, max, distribution, null_frac) }} AS {{name}}
     {% endset %}
     {{ synth_store('base_fields', name, base_field) }}
 
@@ -10,21 +10,28 @@
     {{ dbt_synth_data.synth_store('final_fields', name, final_field) }}
 {%- endmacro %}
 
-{% macro default__synth_column_timestamp_base(min, max, distribution) -%}
+{% macro default__synth_column_timestamp_base(min, max, distribution, null_frac) -%}
     {# NOT YET IMPLEMENTED #}
 {%- endmacro %}
 
-{% macro postgres__synth_column_timestamp_base(min, max, distribution) %}
+{% macro postgres__synth_column_timestamp_base(min, max, distribution, null_frac) %}
     date '{{min}}' + ROUND(RANDOM() * ({% if max|length > 0 %}date '{{max}}'{% else %}CURRENT_DATE{% endif %} - date '{{min}}'))::int
 {% endmacro %}
 
-{% macro snowflake__synth_column_timestamp_base(min, max, distribution) %}
-    dateadd(
-        milliseconds,
-        UNIFORM(
-            0,
-            datediff(milliseconds, '{{min}}'::timestamp, '{{max}}'::timestamp),
-            RANDOM( {{ dbt_synth_data.synth_get_randseed() }} )),
-        '{{min}}'::timestamp
-    )
+{% macro snowflake__synth_column_timestamp_base(min, max, distribution, null_frac) %}
+    {% set date_field %}
+        dateadd(
+            milliseconds,
+            UNIFORM(
+                0,
+                datediff(milliseconds, '{{min}}'::timestamp, '{{max}}'::timestamp),
+                RANDOM( {{ dbt_synth_data.synth_get_randseed() }} )),
+            '{{min}}'::timestamp
+        )
+    {% endset %}
+    CASE
+        WHEN {{ dbt_synth_data.synth_distribution_continuous_uniform(min=0.0, max=1.0) }} < {{null_frac}}
+        THEN NULL
+        ELSE {{ date_field }}
+    END
 {% endmacro%}
--- models/columns_all.sql
+++ models/columns_all.sql
@@ -2,17 +2,15 @@
 
 with
 {{ synth_column_primary_key(name='column_pkey') }}
-{{ synth_column_unique_key(name='column_ukey_std') }}
-{{ synth_column_unique_key(name='column_ukey_null', null_frac=0.2) }}
-
-{{ synth_column_foreign_key(name='column_fkey_std', model_name='stores', column='k_store') }}
-{{ synth_column_foreign_key(name='column_fkey_null', model_name='stores', column='k_store', null_frac=0.2) }}
 
 {{ synth_column_word(name='column_word_std', language_code="en", distribution="weighted", pos=["NOUN", "VERB"]) }}
 {{ synth_column_word(name='column_word_null', language_code="en", distribution="weighted", pos=["NOUN", "VERB"], null_frac=0.2) }}
 
 {{ synth_column_words(name='column_words_std', language_code="en", distribution="uniform", n=5) }}
 {{ synth_column_words(name='column_words_null', language_code="en", distribution="uniform", n=5) }}
+
+{{ synth_column_timestamp(name="column_timestamp_std", min='1938-01-01 00:00:00', max='1994-12-31 23:59:59') }}
+{{ synth_column_timestamp(name="column_timestamp_std", min='1938-01-01 00:00:00', max='1994-12-31 23:59:59', null_frac=0.2) }}
 
 
 
