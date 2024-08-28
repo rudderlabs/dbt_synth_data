487a76f05e18f7bad652f67a7625465a55e9cfde
Gabriel Breahna <gabriel.breahna@gmail.com>
2024-08-28 20:18:04 +0300

    fixed primary key uuid

--- macros/columns/base/primary_key.sql
+++ macros/columns/base/primary_key.sql
@@ -19,6 +19,24 @@
     __row_number
 {% endmacro %}
 
+{% macro snowflake__synth_column_primary_key() -%}
+    UUID_STRING()
+{%- endmacro %}
+
+{% macro redshift__synth_column_primary_key() -%}
+    LPAD(TO_HEX(FLOOR(RANDOM() * (2^32 - 1))::bigint), 8, '0') || '-' ||
+    LPAD(TO_HEX(FLOOR(RANDOM() * (2^16 - 1))::bigint), 4, '0') || '-4' ||
+    LPAD(TO_HEX(FLOOR(RANDOM() * (2^12 - 1))::bigint), 3, '0') || '-' ||
+    LPAD(TO_HEX(FLOOR(RANDOM() * (2^16 - 1))::bigint), 4, '0') || '-' ||
+    LPAD(TO_HEX(FLOOR(RANDOM() * (2^48 - 1))::bigint), 12, '0')
+{%- endmacro %}
+
 {% macro bigquery__synth_column_primary_key() -%}
-    MD5(CAST(__row_number AS STRING))
+    CONCAT(
+        LOWER(FORMAT('%08X', CAST(FLOOR(RAND() * (POW(2, 32) - 1)) AS INT64))), '-',
+        LOWER(FORMAT('%04X', CAST(FLOOR(RAND() * (POW(2, 16) - 1)) AS INT64))), '-4',
+        LOWER(FORMAT('%03X', CAST(FLOOR(RAND() * (POW(2, 12) - 1)) AS INT64))), '-',
+        LOWER(FORMAT('%04X', CAST(FLOOR(RAND() * (POW(2, 16) - 1)) AS INT64))), '-',
+        LOWER(FORMAT('%012X', CAST(FLOOR(RAND() * (POW(2, 48) - 1)) AS INT64)))
+    )
 {%- endmacro %}
